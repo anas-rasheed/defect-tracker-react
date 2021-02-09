2f939cb6ba2faf6d8a678c9d0fb2f6b5ffb827d7
Anas Rasheed <76838052+anas-rasheed@users.noreply.github.com>
2021-02-10 03:27:07 +0530

    initialize the react app and setup configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
