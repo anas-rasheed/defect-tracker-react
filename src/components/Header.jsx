adc6a7768319ca4875a90fa786b9aae74300cd71
Anas Rasheed <76838052+anas-rasheed@users.noreply.github.com>
2021-02-13 04:31:06 +0530

    chore: update header component  snapshot and implement styled component navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,61 @@ const StyledNavbar = styled.nav`
   align-items: center;
   background: linear-gradient(
     to right bottom,
-    rgb(255, 255, 255, 0.7),
-    rgb(255, 255, 255, 0.3)
+    rgba(255, 255, 255, 0.7),
+    rgba(255, 255, 255, 0.3)
   );
-  backdrop-filter: blur(100rem);
-  box-shadow: 0px 6px 20px rgb(157, 160, 252, 1);
+  box-shadow: 0px 6px 20px rgba(157, 160, 252, 1);
+  border-radius: 2rem;
+  position: relative;
+  justify-content: space-evenly;
+`;
+
+const StyledBrand = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  position: absolute;
+  left: 20px;
+  > a {
+    color: #ffffff;
+    text-decoration: none;
+    padding: 10px;
+    background-color: rgba(0, 0, 0, 0.7);
+    border-radius: 2rem;
+  }
+`;
+
+const StyledNavLinks = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  position: absolute;
+  right: 10px;
+  > a {
+    color: #ffffff;
+    text-align: center;
+    margin: 0 5px;
+    width: 50px;
+    text-decoration: none;
+    padding: 10px;
+    background-color: rgba(0, 0, 0, 0.7);
+    border-radius: 2rem;
+  }
 `;
 
 const Header = () => {
   return (
     <>
       <StyledNavbar>
-        <Link to='/postingRouter'>Brand</Link>
+        <StyledBrand>
+          <Link to='/postingRouter'>Brand</Link>
+        </StyledBrand>
+        <StyledNavLinks>
+          <Link to='/'>First</Link>
+          <Link to='/'>Second</Link>
+          <Link to='/'>Third</Link>
+          <Link to='/'>Fourth</Link>
+        </StyledNavLinks>
       </StyledNavbar>
     </>
   );
