d8a2006309e430bb892ed2cdd1c5fae998711b8c
Anas Rasheed <76838052+anas-rasheed@users.noreply.github.com>
2021-02-18 03:17:22 +0530

    downgrade react version to 16.14.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { logout } from '../redux/slices/authSlice';
