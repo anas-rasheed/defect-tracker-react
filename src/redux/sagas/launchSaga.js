87d61a25f2b8793b4903f6e56a3e08afbc8088f8
Anas Rasheed <76838052+anas-rasheed@users.noreply.github.com>
2021-02-28 23:53:55 +0530

    put a temp check for location on saga, needs work for better implementation

--- src/redux/sagas/launchSaga.js
+++ src/redux/sagas/launchSaga.js
@@ -2,5 +2,8 @@ import { put } from 'redux-saga/effects';
 import { getDefects } from '../slices/defectsSlice';
 
 export function* launchSaga() {
-  yield put(getDefects());
+  //TODO: connect the saga with the router to dispatch actions and make api calls on page load
+  if (window.location.href === 'http://localhost:3000/') {
+    yield put(getDefects());
+  }
 }
